3711f14363b72f70f26d3363e19f6410ee816d8a
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-08-27 16:54:28 +0300

    done num 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let final = []
+    for (let i = 0; i < array.length; i++){
+        if(!args.includes(array[i])){
+            final.push(array[i])
+        }
+    }
+    
+ return final
 };
 
 // Do not edit below this line
