cccee47399a2b86082c2f3d82a604af8609df821
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-08-27 18:09:17 +0300

    done 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
 
+    let nums = []
+    let total = 0
+
+    if (first < 0 || last < 0 || typeof first !== 'number' || typeof last !== 'number' || !Number.isInteger(first) || !Number.isInteger(last)){
+        return 'ERROR'
+    }
+
+    let [one, two] = first < last ? [first, last] : [last, first]
+
+    for (let i = one; i <= two; i++){
+        nums.push(i)
+    }
+
+
+    for (let i = 0; i < nums.length; i++){
+  total += nums[i]
+    }
+    return total
 };
 
 // Do not edit below this line
