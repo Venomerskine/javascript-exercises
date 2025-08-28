75de9211b2dd45b335b87bb083421757ed5469ce
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-08-28 06:53:57 +0300

    done 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+  const cleaned = x.replace(/[^\w]|_/g, "").toLowerCase()
+  const reversed = cleaned.split("").reverse().join("")
+  return cleaned ===reversed
 };
 
 // Do not edit below this line
