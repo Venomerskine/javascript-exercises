d950f20a13d3c1607e2f6750bbf7c6b9b726932f
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-08-28 08:35:00 +0300

    done 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(x) {
+     let years = []
+for (let i = 0; i < x.length; i++){
+    let deathYear = x[i].yearOfDeath ? x[i].yearOfDeath : new Date().getFullYear();
+  years.push(deathYear - (x[i].yearOfBirth))
+ }
 
+ let oldest = Math.max(...years)
+
+ return x[years.indexOf(oldest)]
 };
 
 // Do not edit below this line
