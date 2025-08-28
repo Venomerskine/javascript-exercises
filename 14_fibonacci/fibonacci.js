32bfd33d3838fe31e3fb267df08e1569a99944f3
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-08-28 07:34:57 +0300

    peaked at a senior devs working

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,29 +1,17 @@
 const fibonacci = function(x) {
-    fib = []
-
+    
     x = Number(x)
 
-    if (x < 0){
-        return "OOPS"
-    }  else if (x ==0){
-        return 0
-    }
+    if (x < 0) return "OOPS"
+    if (x ==0) return 0
+    if (x == 1) return 1
 
-    if (x == 1){
-        fib = [1]
-    } else if (x >= 2){
-        fib =[1,1]
-    }
+    let a = 1, b = 1
 
-    for(let i = 0; i <= x - 3; i++){
-        
-        num = fib[fib.length -1] + fib[fib.length - 2]
-        fib.push(num)
-        
-    }
-
-    return fib[fib.length - 1]
-
+   for (let i =2; i < x; i++){
+    [a,b] = [b, a + b]
+   }
+ return b
 };
 
 // Do not edit below this line
