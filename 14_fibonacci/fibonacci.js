25d1214b0b9cb9f95987fec8ffca7d63e666081b
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-08-28 07:26:13 +0300

    done 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    fib = []
+
+    x = Number(x)
+
+    if (x < 0){
+        return "OOPS"
+    }  else if (x ==0){
+        return 0
+    }
+
+    if (x == 1){
+        fib = [1]
+    } else if (x >= 2){
+        fib =[1,1]
+    }
+
+    for(let i = 0; i <= x - 3; i++){
+        
+        num = fib[fib.length -1] + fib[fib.length - 2]
+        fib.push(num)
+        
+    }
+
+    return fib[fib.length - 1]
 
 };
 
