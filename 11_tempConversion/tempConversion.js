763ee6333c1cc9e102da94066913ded23512609a
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-08-28 06:16:50 +0300

    done 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fr) {
+  const cel = ((fr - 32) *5)/9
+  return Number(cel.toFixed(1))
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  const fr = parseFloat((cel * 1.8) + 32)
+  return Number(fr.toFixed(1))
 };
 
 // Do not edit below this line
